12cdf91bf4457fdf55a48187b3068089ad510834
Replit user <>
2023-04-12 23:47:23 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -45,7 +45,7 @@ int is_equal(void *key1, void *key2)
 
 void insertMap(HashMap *map, char *key, void *value)
 {
-  int hash = hash(key, map->capacity);
+  long hash = hash(key, map->capacity);
 
   int indice = hash;
   
