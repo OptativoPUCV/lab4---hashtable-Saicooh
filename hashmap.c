79413be0c202211eabd6330810efd270aaea6a53
Replit user <>
2023-04-13 00:07:30 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -29,6 +29,7 @@ long hash(char *key, long capacity)
 {
   unsigned long hash = 0;
   char *ptr;
+  
   for (ptr = key; *ptr != '\0'; ptr++) 
     hash += hash * 32 + tolower(*ptr);
   
@@ -47,7 +48,7 @@ void insertMap(HashMap *map, char *key, void *value)
 {
   long indice = hash(key, map -> capacity);
   
-  while (map -> buckets[indice] != NULL && map -> buckets[indice] -> key != NULL)
+  while (map -> buckets[indice] != NULL )
   {
     if (is_equal(key, map -> buckets[indice] -> key)) return;
     indice = (indice + 1) % map -> capacity;
